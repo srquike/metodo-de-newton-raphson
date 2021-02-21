6c23bc50018b836ea60c9222bb6b4b4d8b439a1e
srquike <srquike@outlook.com>
2021-02-20 19:38:17 -0600

    Agregar archivos de proyecto.

---
+++ Iteracion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodo_de_newton_raphson
+{
+    class Iteracion
+    {
+        private double error;
+
+        public int I { get; set; }
+        public double Xi { get; set; }
+        public double Fx { get; set; }
+        public double Dx { get; set; }
+        public double Xi1 { get; set; }
+        public double Error { get => Math.Round(error, 3); set => error = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace metodo_de_newton_raphson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private IList<Iteracion> iteraciones;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            int i;
+            Iteracion iteracion, iteracionAnterior;
+
+            if (int.TryParse(TxtValorX.Text, out int x) && !string.IsNullOrEmpty(TxtValorX.Text))
+            {
+                iteracionAnterior = new Iteracion();
+                iteraciones = new List<Iteracion>();
+                i = 0;
+
+                do
+                {
+                    iteracion = new Iteracion();
+                    iteracion.I = i;
+
+                    if (iteracion.I == 0)
+                    {
+                        iteracion.Xi = x;
+                    }
+                    else
+                    {
+                        iteracion.Xi = iteracionAnterior.Xi1;
+                    }
+
+                    iteracion.Fx = Math.Pow(Math.E, -iteracion.Xi) - iteracion.Xi;
+                    iteracion.Dx = -Math.Pow(Math.E, -iteracion.Xi) - 1;
+                    iteracion.Xi1 = iteracion.Xi - (iteracion.Fx / iteracion.Dx);
+                    iteracion.Error = Math.Abs((iteracion.Xi1 - iteracionAnterior.Xi1) / iteracion.Xi1) * 100;
+
+                    iteraciones.Add(iteracion);
+                    iteracionAnterior = iteracion;
+                    i++;
+
+                } while (iteracion.Error != 0);
+
+                DgIteraciones.ItemsSource = iteraciones;
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            TxtValorX.Clear();
+            DgIteraciones.ItemsSource = null;
+            TxtValorX.Focus();
+        }
+    }
+}
